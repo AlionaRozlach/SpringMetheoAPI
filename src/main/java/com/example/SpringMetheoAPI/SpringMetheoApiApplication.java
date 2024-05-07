e7fba01ce231345ee241b49fd794f8cd9ed3f0ba
ar617wx <alona.rozlach@student.tuke.sk>
2024-05-07 11:39:05 +0200

    Initial commit

---
+++ src/main/java/com/example/SpringMetheoAPI/SpringMetheoApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringMetheoAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMetheoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMetheoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SpringMetheoAPI/SpringMetheoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringMetheoAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMetheoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
